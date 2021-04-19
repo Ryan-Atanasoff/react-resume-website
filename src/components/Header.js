779767011866d916b9aabc8dbae8a459b1d011d6
Ryan-Atanasoff <ryan.atanasoff@gmail.com>
2021-04-18 20:45:16 -0500

    Optimizing

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,7 @@ import * as Scroll from 'react-scroll';
 
 const Header = ({header_data}) => {
    if (header_data) {
-      var name = header_data.main.first_name;
-      var description = header_data.main.description;
+      var description = "I'm a software engineer based in Madison, Wisconsin. This is my website. Make yourself at home.";
       var networks = header_data.main.social.map((network) => (<SocialIcon key={network.url} url={network.url} className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}} />));
    }
 
@@ -12,7 +11,7 @@ const Header = ({header_data}) => {
    <header id="home" className='header' name='home'>
       <div className="row banner">
          <div className="banner-text">
-            <h1 className="responsive-headline">Hi. I'm {name}.</h1>
+            <h1 className="responsive-headline">Hi. I'm Ryan.</h1>
             <h3><span>{description}</span></h3>
             <br />
             <ul className="social">{networks}</ul>
